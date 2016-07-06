87a4de44f53815c3cdf626912d2d3723852fef0e
rohan-tuli <clickerton@gmail.com>
2016-07-06 14:36:10 -0700

    Getting input from user

--- connect4.cpp
+++ connect4.cpp
@@ -4,6 +4,21 @@
 
 using namespace std;
 
+int gameStatus = 0;
+
+// Gets the input.
+void getInput() {
+	int column;
+	// TODO: check for invalid column values.
+	while ((std::cout << "Which column would you like to place your disk in?\n")
+		&& !(std::cin >> column)) {
+		std::cout << "That's not a number. Please try again.\n";
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+}
+
+// Prints the grid.
 void printGrid() {
 	for (int i = 0; i <= 6; ++i) {
 		cout << " _";
@@ -18,9 +33,13 @@ void printGrid() {
 	for (int i = 0; i <= 6; ++i) {
 		cout << " " << i + 1;
 	}
+	cout << endl;
 }
 	
 int main() {
-	printGrid();
-	return 0;
+	while (gameStatus == 0) {
+		printGrid();
+		getInput();
+	}
+    return 0;
 }
