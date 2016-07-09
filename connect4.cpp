ef12b891d135b95d2d39422210cd869ef5af52ca
rohan-tuli <clickerton@gmail.com>
2016-07-09 11:13:28 -0700

    Added vectors

--- connect4.cpp
+++ connect4.cpp
@@ -1,67 +1,49 @@
 // Connect 4
 
-#include <iostream> 
-#include <string>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
+const int NUM_ROWS = 6;
+const int NUM_COLUMNS = 7;
+const char EMPTY = '.';
+const char PLAYER = 'X';
+const char SPACE = ' ';
+
 int gameStatus = 0;
-int column; // TO"DO: better name.
 
-
-// TODO: fix syntax.
-
-string[] grid = new string[7][6];
-
-
-//Creates the grid.
-void createGrid() {
-	std::vector<std::vector<int>> grid{ {1, 1, 1} }
-	}
-}
+vector<vector<char> > grid(NUM_ROWS, vector<char>(NUM_COLUMNS, EMPTY));
 
 // Gets the input.
 void getInput() {
-	while ((std::cout << "Which column would you like to place your disk in?\n")
-		&& !(std::cin >> column)) {
-		std::cout << "That's not a number. Please try again.\n";
-		std::cin.clear();
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	int column;
+	while ((cout << "Which column would you like to place your disk in? ")
+		&& !(cin >> column)) {
+		cout << "That's not a number. Please try again.\n";
+		cin.clear();
+		cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
+
 	}
+  // TODO: find exact row where this goes.
+
+  grid[0][column - 1] = PLAYER;
+
 }
 
-// Prints the grid.
 void printGrid() {
-	for (int i = 0; i <= 6; ++i) {
-		cout << " _";
-	}
-	cout << endl;
-	for (int i = 0; i <= 5; ++i) {
-		for (int i = 0; i <= 6; ++i) {
-			cout << "|_";
-		}
-		cout << "|" << endl;
-	}
-	for (int i = 0; i <= 6; ++i) {
-		cout << " " << i + 1;
-	}
-	cout << endl;
-}
-	
-void editGrid() {
-	string cat = "cat";
-	string dog = "dog";
-	string row1 = cat + dog;
-	cout << row1;
+  for (int i = 0; i < grid.size(); ++i) {
+    for (int j = 0; j < grid[i].size(); ++j) {
+      cout << grid[i][j] <<  SPACE;
+    }
+    cout << endl;
+  }
 }
 
 int main() {
-	createGrid();
-	grid.size();
-	//while (gameStatus == 0) {
+	while (gameStatus == 0) {
 		printGrid();
 		getInput();
-		editGrid();
-	//}
-    return 0;
+	}
+  return 0;
 }
